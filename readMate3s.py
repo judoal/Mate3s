2b3f0f5c2f23ac02466882a554ded564e06fbeb4
judoal <allengordon@randori-consulting.com>
2022-08-09 14:47:47 -0600

    Update readMate3s.py

--- readMate3s.py
+++ readMate3s.py
@@ -1,123 +1,204 @@
-#!/usr/local/bin/python
+#!/Users/allengordon/opt/miniconda3/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 import http.client
 import time
 import json
 from datetime import datetime
+import sys
+import os
 
-
-
-""" Sample output from MATE3S (dataJobj): 
+"""  
+Sample output from MATE3S (dataJobj): 
 http://192.168.1.7/Dev_status.cgi?&Port=0
 
 {'devstatus': {'Gateway_Type': 'Mate3s', 'Sys_Time': 1600598366, 'Sys_Batt_V': 54.0, 'ports': [{'Port': 1, 'Dev': 'FX', 'Type': '120V', 'Inv_I': 10, 'Chg_I': 0, 'Buy_I': 0, 'Sell_I': 4, 'VAC_in': 127, 'VAC_out': 126, 'Batt_V': 54.0, 'AC_mode': 'AC USE', 'INV_mode': 'Sell', 'Warn': ['Fan Failure'], 'Error': ['none'], 'AUX': 'enabled'}, {'Port': 2, 'Dev': 'FX', 'Type': '120V', 'Inv_I': 0, 'Chg_I': 0, 'Buy_I': 5, 'Sell_I': 0, 'VAC_in': 121, 'VAC_out': 125, 'Batt_V': 53.6, 'AC_mode': 'AC USE', 'INV_mode': 'Comm Error', 'Warn': ['Fan Failure'], 'Error': ['Over Temp'], 'AUX': 'enabled'}, {'Port': 3, 'Dev': 'FX', 'Type': '120V', 'Inv_I': 10, 'Chg_I': 0, 'Buy_I': 0, 'Sell_I': 10, 'VAC_in': 124, 'VAC_out': 126, 'Batt_V': 53.6, 'AC_mode': 'AC USE', 'INV_mode': 'Sell', 'Warn': ['none'], 'Error': ['none'], 'AUX': 'enabled'}, {'Port': 4, 'Dev': 'CC', 'Type': 'MX60', 'Out_I': 20.0, 'In_I': 16, 'Batt_V': 53.4, 'In_V': 70.0, 'Out_kWh': 3.8, 'CC_mode': '  ', 'Error': ['none'], 'Aux_mode': 'Disabled', 'AUX': 'disabled'}, {'Port': 5, 'Dev': 'CC', 'Type': 'MX60', 'Out_I': 15.0, 'In_I': 13, 'Batt_V': 53.2, 'In_V': 66.6, 'Out_kWh': 3.5, 'CC_mode': '  ', 'Error': ['none'], 'Aux_mode': 'Disabled', 'AUX': 'disabled'}, {'Port': 6, 'Dev': 'CC', 'Type': 'MX60', 'Out_I': 9.0, 'In_I': 6, 'Batt_V': 53.1, 'In_V': 91.6, 'Out_kWh': 4.0, 'CC_mode': ' ', 'Error': ['none'], 'Aux_mode': 'Disabled', 'AUX': 'disabled'}, {'Port': 7, 'Dev': 'CC', 'Type': 'MX60', 'Out_I': 17.0, 'In_I': 15, 'Batt_V': 53.3, 'In_V': 64.4, 'Out_kWh': 4.6, 'CC_mode': '  ', 'Error': ['none'], 'Aux_mode': 'Disabled', 'AUX': 'disabled'}]}}
 """
 
+class realTimePlot:
+	def init(a, b, c):
+		global i
+		global xmin
+		global xmax
+		global ymin
+		global ymax
+		i = a
+		xmin = b
+		xmax = c
+		ymin = -1.
+		ymax = 1.
+		plt.axis([xmax, xmin, ymin, ymax])
+
+	def plot(t, val1, val2):
+		tval = t
+		global xmin
+		global xmax
+		y2 = val2
+		y1 = val1
+		#        print (n, y1, y2)
+		if tval > xmax:
+			xmin = xmin + 0.1
+			xmax = xmax + 0.1
+			plt.axis([xmax, xmin, -1., 1.])
+			print("scrolling")
+		plt.scatter(tval, y1, color='black')
+		plt.pause(0.02)
+		plt.show(block=False)
+
+		plt.scatter(tval, y2, color='red')
+		plt.pause(0.02)
+		plt.show(block=False)
+		print(str(tim) + " " + str(y1) + " " + str(y2))
+#        i += 0.1
+
+
+
 #Inverter inits
-invCurrent=[]
-invChgCurrent=[]
-invBuyCurrent=[]
-invSellCurrent=[]
-invVACIn=[]
-invVACOut=[]
-invBattV=[]
-invACMode=[]
-invMode=[]
-invWarn=[]
-invErr=[]
-invAux=[]
+invCurrent=['1','1','1','1','1','1','1','1','1','1']
+invChgCurrent=['1','1','1','1','1','1','1','1','1','1']
+invBuyCurrent=['1','1','1','1','1','1','1','1','1','1']
+invSellCurrent=['1','1','1','1','1','1','1','1','1','1']
+invVACIn=['1','1','1','1','1','1','1','1','1','1']
+invVACOut=['1','1','1','1','1','1','1','1','1','1']
+invBattV=['1','1','1','1','1','1','1','1','1','1']
+invACMode=['1','1','1','1','1','1','1','1','1','1']
+invMode=['1','1','1','1','1','1','1','1','1','1']
+invWarn=['1','1','1','1','1','1','1','1','1','1']
+invErr=['1','1','1','1','1','1','1','1','1','1']
+invAux=['1','1','1','1','1','1','1','1','1','1']
 
 #Charge controller inits
-ccOutCurrent=[]
-ccInCurrent=[]
-ccBattV=[]
-ccInV=[]
-ccKWH=[]
-ccMode=[]
-ccMode=[]
-ccError=[]
-ccAuxMode=[]
-ccAux=[]
+ccOutCurrent=['1','1','1','1','1','1','1','1','1','1']
+ccInCurrent=['1','1','1','1','1','1','1','1','1','1']
+ccBattV=['1','1','1','1','1','1','1','1','1','1']
+ccInV=['1','1','1','1','1','1','1','1','1',","]
+ccKWH=['1','1','1','1','1','1','1','1','1','1']
+ccMode=['1','1','1','1','1','1','1','1','1','1']
+ccError=['1','1','1','1','1','1','1','1','1','1']
+ccAuxMode=['1','1','1','1','1','1','1','1','1','1']
+ccAux=['1','1','1','1','1','1','1','1','1','1']
+ccCount=0
+ofile=""
+hourPrev=""
+webPath=""
+hourNow=""
+conn=""
 
+os.chdir("/Users/allengordon/Documents/workspace/pgsolar/pvlogs")
+#os.chdir("/Users/allengordon/Documents/workspace/pgsolar/pvlogs")
+
+tim = 0
 while (1):
-	conn = http.client.HTTPConnection("192.168.1.7")
-	conn.request("GET", "/Dev_status.cgi?&Port=0")
+	realTimePlot.init(0, 0, 10)
+	now0 = int(time.time())
+	try:
+		conn = http.client.HTTPConnection("192.168.1.7", timeout=300)
+		conn.request("GET", "/Dev_status.cgi?&Port=0")
+		r1 = conn.getresponse()
+		data1 = r1.read()
+		dataDecoded=data1.decode()
+		dataJobj=json.loads(dataDecoded)
+#		print(dataJobj)
+#		print(dataJobj['devstatus']['Sys_Time'])
+	except:
+		print("\n" + "Exception", sys.exc_info()[0], "occurred", flush=True)
+#		continue
+#	print (int(time.time()))
 
-	r1 = conn.getresponse()
-	
-	data1 = r1.read()  # This will return entire content.
-	
-	dataDecoded=data1.decode()
-	dataJobj=json.loads(dataDecoded)
-	
-	print (dataJobj['devstatus']['Sys_Time'])
-	print(str(datetime.now()))
+#	print (dataJobj['devstatus']['Sys_Time'])
+	now = datetime.now()
+	hourNow=now.today().hour
+	minNow=now.today().minute
+	secNow=now.today().second
+	format="%Y_%m_%d"
+	date1=now.strftime(format)
+	dateTimeStr=str(hourNow) + "_" + str(minNow) + "_" + str(secNow)
+	print (date1 + "\t" + dateTimeStr, flush=True)
+
+	#write to files
+
+	pathname = date1 + "_PV.log"
+	ofile=open(pathname, "a+", 10)
+	#webpath = "/Users/allengordon/tdmp"
+	#wfile = open("/Users/allengordon/tmp/PV.log", "a+", 10)
 	dataList = list(dataJobj['devstatus']['ports'])  #List
 	for i in range (len(dataList)):
 		dataList = list(dataJobj['devstatus']['ports'])  #List
-#		print (dataList[i])
-#		print ("index: " + str(i))
 		portnum = dataList[i]['Port']
-#		print ("PORTNUM: " + str(portnum))
 
 		if dataList[i]['Dev'] == 'FX':
-			invCurrent.append (str(dataList[i]['Inv_I']))
-			invChgCurrent.append(str(dataList[i]['Chg_I']))
-			invBuyCurrent.append(str(dataList[i]['Buy_I']))
-			invSellCurrent.append(str(dataList[i]['Sell_I']))
-			invVACIn.append(str(dataList[i]['VAC_in']))
-			invVACOut.append(str(dataList[i]['VAC_out']))
-			invBattV.append(str(dataList[i]['Batt_V']))
-			invACMode.append(str(dataList[i]['AC_mode']))
-			invMode.append(str(dataList[i]['INV_mode']))
-			invWarn.append(str(dataList[i]['Warn']))
-			invErr.append(str(dataList[i]['Error']))
-			invAux.append(str(dataList[i]['AUX']))
-			ccOutCurrent.append('')   #padding
-			ccInCurrent.append('')   #padding]
-			ccBattV.append('')   #padding
-			ccInV.append('')   #padding
-			ccKWH.append('')   #padding
-			ccMode.append('')   #padding
-			ccError.append('')   #padding
-			ccAuxMode.append('')   #padding
-			ccAux.append('')   #padding]
+			invCurrent[i]=(str(float(dataList[i]['Inv_I'])))
+			invChgCurrent[i]=(str(dataList[i]['Chg_I']))
+			invBuyCurrent[i]=(str(dataList[i]['Buy_I']))
+			invSellCurrent[i]=(str(dataList[i]['Sell_I']))
+			invVACIn[i]=(str(dataList[i]['VAC_in']))
+			invVACOut[i]=(str(dataList[i]['VAC_out']))
+			invBattV[i]=(str(dataList[i]['Batt_V']))
+			invACMode[i]=(str(dataList[i]['AC_mode']))
+			invMode[i]=(str(dataList[i]['INV_mode']))
+			invWarn[i]=(str(dataList[i]['Warn']))
+			invErr[i]=(str(dataList[i]['Error']))
+			invAux[i]=(str(dataList[i]['AUX']))
 
-			print ("INV #" +  str(i) + " CURRENT: " + invCurrent[i]+ " AMPS")
-			print ("INV #" + str (i) + " CHARGER: "  + invChgCurrent[i] + " AMPS")
-			print ("INV #" + str (i) + " BUY: "  + invBuyCurrent[i] + " AMPS")			
-			print ("INV #" + str (i) + " SELL: "  + invSellCurrent[i] + " AMPS")
-			print ("INV #" + str (i) + " VAC IN: "  + invVACIn[i] + " VOLTS")
-			print ("INV #" + str (i) + " VAC OUT: "  + invVACOut[i] + " VOLTS")
-			print ("INV #" + str (i) + " BATT VOLT: "  + invBattV[i] + " VOLTS")			
-			print ("INV #" + str (i) + " AC MODE: "  + invACMode[i])
-			print ("INV #" + str (i) + " INV MODE: " + invMode[i])
-			print ("INV #" + str (i) + " INV WARN: "  + invWarn[i])
-			print ("INV #" + str (i) + " INV ERROR: " + invErr[i])		
-			print ("INV #" + str (i) + " INV AUX: "  + invAux[i])
-			print ("\n")
+			if i == 0:
+					ofile.write(date1 + ":" + dateTimeStr)
+					#wfile.write(dateTimeStr)
+					InvHeaderStr="INV\t" + "I\t" + "IChg\t" + "Ibuy\t" + "Isel\t" + "Vacin\t" + "VacOut\t" +  \
+								 "Vbatt\t" + "ACmode\t" + "WARN\t\t" + "ERR\t\t" + "Aux"
+					print (InvHeaderStr, flush=True)
+					ccCount = 0
+
+					invFileHeaderStr = InvHeaderStr
+					ofile.write("\n"+ InvHeaderStr + "\n")
+					#wfile.write("\n"+ InvHeaderStr + "\n")
+
+			invStr = str(i) + "\t" + str(invCurrent[i]) + "\t" + str(invChgCurrent[i]) + "\t" + \
+					 str(invBuyCurrent[i]) + "\t" + str(invSellCurrent[i]) \
+					 + "\t" + str(invVACIn[i]) + "\t" + str(invVACOut[i]) + "\t" + str(invBattV[i]) + "\t" + \
+					 str(invMode[i]) + "\t" + str(invWarn[i]) + "\t" + str(invErr[i]) + "\t" + str(invAux[i])
+
+			print(invStr, flush=True)
+			invFileStr=invStr
+			ofile.write (invFileStr + "\n")
 
 		if dataList[i]['Dev'] == 'CC':
-			ccOutCurrent.append(str(dataList[i]['Out_I']))
-			ccInCurrent.append(str(dataList[i]['In_I']))
-			ccBattV.append(str(dataList[i]['Batt_V']))
-			ccInV.append(str(dataList[i]['In_V']))
-			ccKWH.append(str(dataList[i]['Out_kWh']))
-			ccMode.append(str(dataList[i]['CC_mode']))
-			ccError.append(str(dataList[i]['Error']))
-			ccAuxMode.append(str(dataList[i]['Aux_mode']))
-			ccAux.append(str(dataList[i]['AUX']))
-		
-			print ("CC #" + str(i)  + " CC OUTPUT CURRENT: " + ccOutCurrent[i] + " AMPS") 
-			print ("CC #" + str(i)  + " CC INPUT CURRENT: " + ccInCurrent[i] + " AMPS")
-			print ("CC #" + str(i)  + " CC BATT VOLT: " + ccBattV[i]  + " VOLTS")
-			print ("CC #" + str(i)  + " CC INPUT VOLT: " + ccInV[i] + " VOLTS")
-			print ("CC #" + str(i)  + " CC KWH: " +  ccKWH[i] + " kWh")
-			print ("CC #" + str(i)  + " CC MODE: " + ccMode[i])
-			print ("CC #" + str(i)  + " CC ERROR: " + ccError[i])
-			print ("CC #" + str(i)  + " CC AUX Mode: " + ccAuxMode[i])
-			print ("CC #" + str(i)  + " CC AUX: " + ccAux[i])
-			
-			print("\n")
+			ccOutCurrent[i]=(str(dataList[i]['Out_I']))
+			ccInCurrent[i]=(str(dataList[i]['In_I']))
+			ccBattV[i]=(str(dataList[i]['Batt_V']))
+			ccInV[i]=(str(dataList[i]['In_V']))
+			ccKWH[i]=(str(dataList[i]['Out_kWh']))
+			ccMode[i]=(str(dataList[i]['CC_mode']))
+			ccError[i]=(str(dataList[i]['Error']))
+			ccAuxMode[i]=(str(dataList[i]['Aux_mode']))
+			ccAux[i]=(str(dataList[i]['AUX']))
+
+			if ccCount == 0:
+					ccHeaderStr ="\n" + "CC\t" + "Iout\t" + "Iin\t" + "Vb\t" + "Vin\t" + "KWH\t" + \
+								 "Mode\t" + "Err\t\t" + str("AuxMode\t") + "Aux"
+					print (ccHeaderStr, flush=True)
+					ccCount=-1
+
+					ccFileHeaderStr = ccHeaderStr
+					ofile.write("\n" + ccHeaderStr + "\n")
+
+			ccStr = str(i) + "\t" + str(ccOutCurrent[i]) + "\t" + str(ccInCurrent[i]) + "\t" + \
+					str(ccBattV[i]) + "\t" + str(ccInV[i]) + "\t" + str(ccKWH[i]) + "\t" + ccMode[i] + \
+					"\t" + str(ccError[i]) + "\t" + ccAuxMode[i] + "\t" + ccAux[i]
+			print (ccStr)
+
+			ccFileStr = ccStr
+			ofile.write(ccFileStr + "\n")
+			#wfile.write(ccFileStr + "\n")
+			ofile.flush()
+			#wfile.flush()
+
+	realTimePlot.plot(tim, ccOutCurrent[3],ccOutCurrent[4])
+	print(tim, ccOutCurrent[3],ccOutCurrent[4] )
+	tim += 1
+	print("\n", flush=True)
+	dataList.clear()
+	ofile.close()
 
 	time.sleep(1)
 	conn.close()
